67427c2bb5404d692630fdfc424468798d2a7fac
TasyaKh <tasya132002@gmail.com>
2024-08-07 09:44:22 +0800

    create expand elem down

--- src/components/elements/buttons/BtnArrowDown/BtnArrowDown.tsx
+++ src/components/elements/buttons/BtnArrowDown/BtnArrowDown.tsx
@@ -3,14 +3,14 @@ import './BtnArrowDown.scss';
 
 
 interface Props {
-
+    onExpand?:()=>void
 }
 
-export const BtnArrowDown: FC<Props> = () => {
+export const BtnArrowDown: FC<Props> = ({onExpand}) => {
 
     return (
         <div className={"btn-arrow-down"}>
-            <div className={"btn-icon"}>
+            <div className={"btn-icon"} onClick={onExpand}>
                 <img src={"img/assets/icons/arrow-down.svg"} alt={"arrow-down"}/>
             </div>
         </div>
--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -1,70 +1,51 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './ReviewsPage.scss';
 import {Navbar} from "../../components/Navbar/Navbar";
 import {Footer} from "../../components/Footer/Footer";
 import {Comment} from "./Comment/Comment";
-import {BtnArrowDown} from "../../components/elements/buttons/BtnArrowDown/BtnArrowDown";
+import {getReviewsPage,} from "../../api/pages";
+import {ExpandableElem} from "../../components/ExpandableElem/ExpandableElem";
 
 interface Props {
 }
 
 export const ReviewsPage: FC<Props> = () => {
 
-    const comments = [
-        {
-            id: 1,
-            avatar: "img/reviews/img_1.png",
-            author: `Олег Комаров \n front-end разработчик`,
-            text:
-                `Стажировка в true.code погрузила меня в настоящую рабочую атмосферу — благодаря опытным наставникам я быстро изучил работу с современными инструментами,
-                 новыми стандартами JavaScript 
-                и методологией написания кода, как правильно подобрать ту или иную технологию для решения конкретной задачи.\n\n
-                Кроме того, достаточно скоро я усвоил, что такое работа в команде и написание кода, который не просто работает, но и понятен другим специалистам.`,
-            isAuthorLeftSide: true
+    const [data, setData] = useState<any>()
 
-        },
-        {
-            id: 2,
-            avatar: "img/reviews/img.png",
-            author: "Екатерина Иванова \n " +
-                "менеджер проектов",
-            text:
-                `В декабре 2019 увидела пост о наборе стажеров. Прошла собеседование, сделала тестовое, и меня пригласили на стажировку. Стажировка длилась 3 месяца. Практически с самого начала давали боевые реальные задачи, сначала небольшие и попроще, постепенно всё более ответственные. Ещё было много теории: менеджерский курс, статьи и лекции по основным понятиям дизайна, верстки и разработки, курсы Битрикс. Особенно понравилось, 
-                что до карантина каждую неделю кто-нибудь из ребят читал лекцию. Например, было несколько маркетинговых и дизайнерских.
-Адаптация в коллективе прошла довольно легко, все ребята очень открытые и отзывчивые.
+    useEffect(() => {
+        getReviews()
+    }, []);
 
-Стажировка закончилась в конце марта, и в апреле мне предложили продолжить работу уже полноценным проджект-менеджером.`,
-            isAuthorLeftSide: false
-        },
+    const getReviews = async () => {
+        const d = await getReviewsPage()
+        setData(d)
+    }
 
-    ]
+
     return (
         <div className="reviews-page">
             {/*<div className={"container-full"}>*/}
             <Navbar/>
             <div className={"reviews-wrapper container"}>
                 <h3 style={{textAlign: "center"}}>
-                    Наши отзывы
+                    {data?.header}
                 </h3>
 
                 <div className={"reviews"}>
-                    <div className={"row comments"}>
-                        {comments && comments.map((el) => (
-                            <div className={"col-12"}>
-                                <Comment text={el.text} author={el.author} avatar={el.avatar}
-                                         isAuthorLeftSide={el.isAuthorLeftSide}/>
-                            </div>
-                        ))}
-                    </div>
-
-                    {/*buttons*/}
-                    <div className={"row justify-content-center"}>
-                        <div className={"arrow-down"}>
-                            <div className={" d-flex justify-content-center"}>
-                                <BtnArrowDown/>
-                            </div>
+                    <ExpandableElem child={
+                        <div className={"row comments"}>
+                            {data?.comments && Object.keys(data.comments).map((key, index) => (
+                                <div className={`col-12 invisible`} id={`elem-${index}`}>
+                                    <Comment text={data.comments[key].text} author={data.comments[key].author_name}
+                                             avatar={data.comments[key].avatar}
+                                             isAuthorLeftSide={data.comments[key].is_author_left_side}/>
+                                </div>
+                            ))}
                         </div>
-                    </div>
+
+                    } expandCount={2} visibleCount={2}/>
+
                 </div>
             </div>
 
@@ -78,14 +59,12 @@ export const ReviewsPage: FC<Props> = () => {
 
             {/*experience*/}
             <div className={"img-experience mobile-hide"}>
-                <img src={"/gif/experience.gif"}
-                     className={"img-resizable"} alt={""}/>
-
-                {/*</div>*/}
+                <div className={"animate-circle"}>
+                    <img className={"img-contain"} src={"img/animate/experience.png"} alt={""}/>
+                </div>
             </div>
-            <Footer/>
-            {/*</div>*/}
 
+            <Footer/>
 
         </div>
     )
