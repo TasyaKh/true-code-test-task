4093b07223c7b1bb71b4ae53027a4ac5ee70e49f
TasyaKh <tasya132002@gmail.com>
2024-08-07 18:00:11 +0800

    form page created, phone link added on pages

---
+++ src/pages/FormPage/FormPage.tsx
@@ -0,0 +1,42 @@
+import React, {FC, useEffect, useState} from 'react';
+import './ErrorPage.scss';
+import {Form} from "../../components/Form/Form";
+import {getMainPageContent} from "../../api/pages";
+import {Footer} from "../../components/Footer/Footer";
+
+interface Props {
+}
+
+export const FormPage: FC<Props> = () => {
+
+    const [data, setData] = useState<any>()
+
+    useEffect(() => {
+        getMainPage()
+    }, []);
+
+    const getMainPage = async () => {
+        const d = await getMainPageContent()
+        setData(d)
+    }
+
+    return (
+        <div className="form-page">
+            <div className="container">
+                <div className="form-page-wrapper">
+                    <div className={"form-elem"} style={{width: "100%"}}>
+                        <div className={"form-header"}>{data?.header_form}</div>
+                        <Form/>
+                    </div>
+                </div>
+            </div>
+
+            <div className={"img-gauss mobile-hide"}>
+                <img className={"img-resizable"} src={"/img/gauss.png"} alt={""}/>
+            </div>
+
+            <Footer/>
+        </div>
+    )
+};
+
